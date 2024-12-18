a22d5255658ad21d580a95b1c27f39cbadf9878f
rahul <rahulpdy.pandey@gmail.com>
2024-12-18 12:55:43 +0530

    css update

--- components/ui/Events/EventCard.jsx
+++ components/ui/Events/EventCard.jsx
@@ -12,14 +12,14 @@ const EventCard = ({ id, title, date, time, host, location, price }) => {
         <div className="title">{title}</div>
         <div className="date">
           {date}
-          <span>Started at {time}</span> 
+          <span className='px-1'>Started at {time}</span> 
         </div>
         <div className="hosted">
           Hosted by <span>{host}</span>
         </div>
         <div className="location">
           <img src="./images/location.svg" alt="Location" />
-          <span>{location}</span>
+          <span className='mx-2'>{location}</span>
         </div>
         <div className="from">From {price}</div>
       </div>
