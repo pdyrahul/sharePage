c8eed39155e0fc3acf0b755f97bdd260175021fe
rahul <rahulpdy.pandey@gmail.com>
2024-12-19 23:30:33 +0530

    myevent custom page

--- app/(dashboard)/my-events/page.jsx
+++ app/(dashboard)/my-events/page.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-
+import PhotosCard from '@/components/PhotosCard';
 function MyEventPage(props) {
     return (
-        <div>MyEventPage</div>
+
+        <div className="event-body" style={{
+            boxShadow: '1px 1px 4px 0px #80808075'
+          }}>
+            <div className="heading">My Events</div>
+            <PhotosCard />
+            <PhotosCard />
+          </div>
+
+
     );
 }
 
