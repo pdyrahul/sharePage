4ece9dfbd46020b00ce78ec35f6d70580a6fa3d2
rahul <rahulpdy.pandey@gmail.com>
2025-01-11 13:02:37 +0530

    ref1

--- app/(dashboard)/event-managing/submit-event/component/TicketList.jsx
+++ app/(dashboard)/event-managing/submit-event/component/TicketList.jsx
@@ -8,7 +8,7 @@ const TicketList = ({ name, setTicketList }) => {
       {({ push, remove, form }) => (
         <div className='mt-3 w-100'>
           {form.values[name]?.map((_, index) => (
-            <div key={index} style={{ marginBottom: '10px', display: 'flex', alignItems: 'center', justifyContent:"space-between"}}>
+            <div key={index} style={{ marginBottom: '10px', display: 'flex', alignItems: 'center', justifyContent:"center"}}>
               <div style={{ marginRight: '10px' }}>
                 <label htmlFor={`${name}[${index}].Ticket_type`}>Ticket Type:</label>
                 <Field name={`${name}[${index}].Ticket_type`} placeholder="Type" />
