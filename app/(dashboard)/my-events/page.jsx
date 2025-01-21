9b62a9d76e162cced7b3b165e645ad80f3a510a5
rahul <rahulpdy.pandey@gmail.com>
2025-01-21 21:00:55 +0530

    fix

--- app/(dashboard)/my-events/page.jsx
+++ app/(dashboard)/my-events/page.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import PhotosCard from '../../../components/PhotosCard';
-function MyEventPage(props) {
+function MyEventPage() {
     return (
 
         <div className="event-body" style={{
             boxShadow: '1px 1px 4px 0px #80808075'
           }}>
             <div className="heading">My Events</div>
-            <PhotosCard />
-            <PhotosCard />
+            events
           </div>
 
 
