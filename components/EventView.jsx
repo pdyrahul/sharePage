89215166293040993fbe04f75c830766bcf19aa3
rahul <rahulpdy.pandey@gmail.com>
2025-01-22 23:25:33 +0530

    public1

--- components/EventView.jsx
+++ components/EventView.jsx
@@ -137,13 +137,13 @@ const EventView = ({ slug }) => {
                             setFieldValue('refundPolicy', event.refundPolicy || '', false);
                             setFieldValue('endTime', event.endTime || '', false);
                             setFieldValue('ticketUrl', event.ticketUrl || '', false);
-                            setFieldValue('isFeatured', event.isFeatured ? "1" : "2", false); // Assuming you use 'Yes'/'No' for form
+                            setFieldValue('isFeatured', event.isFeatured ? "Yes" : "No", false); // Assuming you use 'Yes'/'No' for form
                             setFieldValue('eventType', event.event_type === 'paid' ? 'paid' : 'free', false);
 
                             // Handle nested fields
                             setFieldValue('ethnicity', event.ethnicity.id, false);
                             setFieldValue('category', event.category.idspevent, false);
-                            setFieldValue('sponsor', event.sponsor.sponsorName, false);
+                            setFieldValue('sponsor', event.sponsor.id, false);
                             // Set description and amenities which are HTML strings
                             setFieldValue('description', event.description, false);
                             setFieldValue('amenities', event.amenities, false);
