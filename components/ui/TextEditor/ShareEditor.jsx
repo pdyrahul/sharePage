c7179cace173ab6bda08fe23a3cfb6cd895f96ea
Mayaram yadav <mmayaramyadav@gmail.com>
2025-01-24 18:17:14 +0530

    update

--- components/ui/TextEditor/ShareEditor.jsx
+++ components/ui/TextEditor/ShareEditor.jsx
@@ -1,52 +1,11 @@
-"use client"
-import { useCallback, useState, useRef } from 'react';
-import { debounce } from 'lodash';
-import dynamic from 'next/dynamic';
 
-const JoditEditor = dynamic(() => import('jodit-react'), {
-  ssr: false,
-});
-const ShareEditor = ({ data, setData }) => {
-  const [content, setContent] = useState(data);
-  const editor = useRef(null);
-  const debouncedUpdate = useCallback(
-    debounce((newContent) => {
-      setData(newContent);
-    }, 300),
-    [] // Dependencies array
-  );
 
-  const handleBlur = (newContent) => {
-    setContent(newContent);
-    debouncedUpdate(newContent);
-  };
+const ShareEditor = ({ content }) => {
 
   return (
-    <div style={{ width: '100%' }}>
-      <JoditEditor
-        ref={editor}
-        value={content}
-        onBlur={handleBlur} // Use onBlur for state update
-        config={{
-          readonly: false,
-          height: 150,
-          toolbarSticky: false,
-          askBeforePasteHTML: false,
-          askBeforePasteFromWord: false,
-          defaultActionOnPaste: 'insert_as_html',
-          buttons: [
-            'undo', 'redo', '|',
-            'bold', 'italic', 'underline', '|',
-            'copy', 'cut', 'paste', '|',
-            'align', 'font', 'fontsize',
-          ],
-          clipboard: {
-            matchVisual: true, // Helps in ensuring pasted content matches the editor style
-          },
-          placeholder: 'Start typing or paste your content here...',
-          allowPasteFromWord: true, // Allows pasting from Word or other editors
-        }}
-      />
+    <div className="share-editor">
+      <h2>Share Editor</h2>
+
     </div>
   );
 };
