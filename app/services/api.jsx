233ea263ea238d820e5f170a0eca52c377928819
rahul <rahulpdy.pandey@gmail.com>
2025-01-29 22:24:28 +0530

    categoryset

--- app/services/api.jsx
+++ app/services/api.jsx
@@ -3,12 +3,12 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
-    "Content-Type": "multipart/form-data", 
+    "Content-Type": "multipart/form-data",
     'Content-Type': 'application/json',
   },
 });
 // export const userLogin = axios.post("/login");
-export const saveEvent= (submitData)=>api.post("/event", submitData);
+export const saveEvent = (submitData) => api.post("/event", submitData);
 export const getEventCategories = () => api.get("/event/ethnicity-category");
 export const getSponsors = () => api.get("/event/sponsored");
 export const createSponsor = (data) => api.post("/event/sponsored", data);
@@ -19,10 +19,11 @@ export const setProfile = (userId) => api.get(`/event/user/set-profile/${userId}
 export const getEventList = () => api.get("/event");
 export const getDraftList = () => api.get("/event?status=1");
 export const getActiveList = () => api.get("/event?status=2");
-export const postAction = (payload) =>api.post("/event/favorite", payload);
+export const postAction = (payload) => api.post("/event/favorite", payload);
 export const getEventBySlug = (slug) => api.get(`/event/${slug}`);
 export const updateEvent = (slug, data) => api.put(`/event/${slug}`, data);
 export const deletegalleryImage = (galleryId) => api.delete(`/event/delete-gallery-image/${galleryId}`);
+export const getCategory = () => api.get("/event/category");
 export const publicEvent = (filter) => {
   const url = filter ? `/event/latest-events?${filter}=true` : `/event/latest-events`;
   return api.get(url);
@@ -30,4 +31,3 @@ export const publicEvent = (filter) => {
 
 
 export default api;
- 
